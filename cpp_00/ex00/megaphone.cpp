a3a8e3ef5b38647ac76c8c828c2bd46d4e6fd265
Lucas Aubry <aubrylucas.pro@gmail.com>
2024-08-30 22:54:57 +0200

    +| finish

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,25 +6,22 @@
 /*   By: laubry <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 21:45:21 by laubry            #+#    #+#             */
-/*   Updated: 2024/08/30 22:11:41 by laubry           ###   ########.fr       */
+/*   Updated: 2024/08/30 22:37:03 by laubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iostream"
-#include "cctype"
 
 int	main(int argc ,char **argv)
 {
-	if (argc > 1)
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *";
+	else
 	{
-		for (int i = 1; i < argc; i++)
-		{
+		for (int i = 1; i < argc; i++) {
 			for (int j = 0; argv[i][j] != '\0'; j++)
-				std::cout << (char)toupper(argv[i][j]) << std::endl;
-			i++;
+				std::cout << (char) toupper(argv[i][j]);
 		}
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *" << std::endl;
-	return (0);
+	std::cout << std::endl;
 }
